3b09e67b2d280fdda28a808c83c1f8a876b67239
Ilka Carmo <ilka_2009@ig.com.br>
2020-09-28 21:41:36 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using tabuleiro;
- 
+using xadrez;
 namespace xadrez_console
 {
     class Tela
@@ -25,11 +25,19 @@ namespace xadrez_console
 
         }
 
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
+        }
+
         public static void imprimirPeca(Peca peca)
         {
             if(peca.cor == Cor.Branca)
             {
-                Console.WriteLine(peca);
+                Console.Write(peca);
             }
             else
             {
--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -17,5 +17,10 @@ namespace tabuleiro
             this.qteMovimento = 0;
         }
 
+        public void incrementarQteMovimentos()
+        {
+            qteMovimento++;
+        }
+
     }
 }
