4426f7c74ea8ddfc64f3ef443a2e7a7b9bbe5617
Ilka Carmo <ilka_2009@ig.com.br>
2020-10-01 22:10:31 -0300

    Implementando os movimentos possiveis do Rei

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
 using tabuleiro;
+ 
 
 namespace  xadrez
 {
     class Rei : Peca
     {
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor) { 
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor) {
         }
-              public override string ToString(){
+        public override string ToString() {
             return "R";
         }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+        public override bool[,] movimentosPossiveis() {
+
+        bool [,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima 
+
+      }
+
     }
 }
